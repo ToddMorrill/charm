d26db18dc530dcb399fa6b0d545fda83b52494ff
Todd Morrill <tmorrill12@gmail.com>
2023-01-22 15:18:03 -0500

    gdrive uploader, installed CCU1.0

--- google-drive/upload.py
+++ google-drive/upload.py
@@ -1,10 +1,13 @@
 from __future__ import print_function
+import os
+import argparse
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
 
 import utils
+
 
 def create_folder(parents=[]):
     """ Create a folder and prints the folder ID
@@ -34,7 +37,8 @@ def create_folder(parents=[]):
         print(F'An error occurred: {error}')
         return None
 
-def upload_basic():
+
+def upload_basic(filepath, parents=[]):
     """Insert new file.
     Returns : Id's of the file uploaded
 
@@ -48,11 +52,12 @@ def upload_basic():
         # create drive api client
         service = build('drive', 'v3', credentials=creds)
 
-        file_metadata = {'name': 'ccu-619.png'}
-        media = MediaFileUpload('ccu-619.png',
-                                mimetype='image/png')
+        path, filename = os.path.split(filepath)
+        file_metadata = {'name': filename, 'parents': parents}
+        media = MediaFileUpload(filepath, mimetype='image/png')
         # pylint: disable=maybe-no-member
-        file = service.files().create(body=file_metadata, media_body=media).execute()
+        file = service.files().create(body=file_metadata,
+                                      media_body=media).execute()
         print(F'File ID: {file.get("id")}')
 
     except HttpError as error:
@@ -62,6 +67,22 @@ def upload_basic():
     return file.get('id')
 
 
+def main(args):
+    # TODO: check if there is an upload in progress and resume progress using
+    # the meta_file. Create meta_file if it doesn't exist.
+    # TODO: list out all files and compute diff against meta_file
+    # TODO: upload files and respect folder structure
+    
+    folder_id = create_folder(['1zbGQcYl63sREkVm0v-JkhEi6gbNr_tLj'])
+    file_id = upload_basic('./ccu-619.png', parents=[folder_id])
+
 if __name__ == '__main__':
-    # upload_basic()
-    create_folder(['1zbGQcYl63sREkVm0v-JkhEi6gbNr_tLj'])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--meta-file',
+                        help=('The metadata file containing information about '
+                        'the upload.'))
+    parser.add_argument('--directory',
+                        help=('The parent directory containing '
+                              'subdirectories and files to be uploaded.'))
+    args = parser.parse_args()
+    main(args)
